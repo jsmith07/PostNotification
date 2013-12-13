286dde4ecf676608b2ba634d081988bf2ecd248e
jsmith <jsmith@localhost.localdomain>
2013-12-13 15:51:30 -0600

    Cleaning up code formatting

--- snippets.php
+++ snippets.php
@@ -2,8 +2,8 @@
 <!-- Add subscription Form  -->
 <h2>Get notified of new posts:</h2>
 <form id="newsletter" method="post" action="<?php echo post_notification_get_link(); ?>" style="text-align:left">
-	<p>email: <input type="text" name="addr" size="25" maxlength="50" value="<?php echo post_notification_get_addr(); ?>"/> </p>
-	<input type="submit" name="submit" value="Submit" /></p>
+    <p>email: <input type="text" name="addr" size="25" maxlength="50" value="<?php echo post_notification_get_addr(); ?>"/> </p>
+    <input type="submit" name="submit" value="Submit" /></p>
 
 </form>
 
